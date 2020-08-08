4de2fe462e8db3dbca1b75e977033444206c9f04
zzrjezz <58676425+zzrjezz@users.noreply.github.com>
2020-08-08 14:38:20 +0800

    Add files via upload

---
+++ library/ES6/moduleUse.js
@@ -0,0 +1,4 @@
+import {obj,arr} from "./myModuls.js";
+
+console.log(obj);
+console.log(arr);
---
+++ library/ES6/myModule.js
@@ -0,0 +1,11 @@
+export let obj={
+    name:"tom",
+    age:18
+}
+
+let arr=[0,1,2,3];
+
+
+
+export {arr};
+console.log("i am myMoudle");
